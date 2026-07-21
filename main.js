fd6e229fa629a9bd9bbee9b16f4e20f2532c5e69
Nhlamulo <nhlamulob7@gmail.com>
2026-07-21 16:56:21 +0200

    added js functionality for powerstats

--- main.js
+++ main.js
@@ -16,3 +16,30 @@ function hideSidebar() {
  function submitBtn() {
       navigator.vibrate(70)
     }
+
+
+function updateText() {
+    const fill1 = document.getElementById('fill1');
+    const fill2 = document.getElementById('fill2');
+    const fill3 = document.getElementById('fill3');
+    const fill4 = document.getElementById('fill4');
+    const fill5 = document.getElementById('fill5');
+    const fill6 = document.getElementById('fill6');
+
+    const intelligence = document.getElementById('intelligence');
+    const strength = document.getElementById('strength');
+    const speed = document.getElementById('speed');
+    const durability = document.getElementById('durability');
+    const power = document.getElementById('power');
+    const combat = document.getElementById('combat');
+
+    intelligence.textContent = `Intelligence: ${fill1.style.width}`;
+    strength.textContent = `Strength: ${fill2.style.width}`;
+    speed.textContent = `Speed: ${fill3.style.width}`;
+    durability.textContent = `Durability: ${fill4.style.width}`;
+    power.textContent = `Power: ${fill5.style.width}`;
+    combat.textContent = `Combat: ${fill6.style.width}`;
+
+}
+
+updateText()
